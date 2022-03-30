b191f0b28f8fbc7deb80452a28e2b4f08862d747
vinicius-cassaro <vinicius_cassaro@hotmail.com>
2022-03-30 17:27:02 -0300

    feat: create base project with Java and Spring

---
+++ src/main/java/control/ControlFinancialApplication.java
@@ -0,0 +1,13 @@
+package control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlFinancialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/control/ControlFinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
